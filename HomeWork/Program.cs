c8e4c40e0a3511241bf0c016638a8c3d7d810aa8
Valeria <valeriakuz92@mail.ru>
2022-11-18 23:03:08 +0500

    Saved homework

---
+++ HomeWork/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.Clear();
+
+// Задача 2.
+// Программа, получающая на вводе два числа, а на выходе показывающая максимальное значение
+
+// Console.Write("Enter the number1:"); //Консоль пишет:
+// int number1 = Convert.ToInt32( Console.ReadLine()); //Консоль считывает введённое число
+// Console.Write("Enter the number2:"); //после этого консоль запрашивает второе число
+// int number2 = Convert.ToInt32( Console.ReadLine()); //Вводим второе
+// if(number1 >= number2)
+// {
+// Console.WriteLine("The max number is:" + number1);
+// }
+// else
+// Console.WriteLine("The max number is:" + number2);
+
+// Задача 4
+// Нахождение максимального числа из трёх чисел
+
+// int number1 = new Random().Next(1,1000001);
+// int number2 = new Random().Next(1,1000001);
+// int number3 = new Random().Next(1,1000001);
+// Console.WriteLine(number1);
+// Console.WriteLine(number2);
+// Console.WriteLine(number3);
+// int max = number1;
+
+// if (number2 >= max) max = number2;
+// if (number3 >= max) max = number3;
+
+// Console.WriteLine("Maximum is: " + max);
+
+// Задача 6
+// Задача на определение чётного и нечётного числа
+
+
+// Console.Write("Enter the number:");
+// int number = Convert.ToInt32( Console.ReadLine());
+// if (number %2!=0)
+// {
+// Console.Write("Число " + number + " нечетное");
+// }
+// else
+// Console.WriteLine("Число " + number + " четное");
+
+// Задача 8
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+
+// Console.Write("Enter the number:");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int i = 2;
+// Console.WriteLine(N);
+
+// if (N<2)
+// {
+// Console.Write("False, the numbber must to be large"); 
+
+// }
+// else
+
+// while (i <= N)
+// {
+// Console.Write(" " + i);
+// i+=2;
+// }
